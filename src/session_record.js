5f0b0b3e23667946a8ecdae0b57c3b737972b144
Ramon Victor Cardoso <ramonvc19@gmail.com>
2024-08-18 19:53:48 -0300

    refactor: Remove console statements for closing and opening sessions

--- src/session_record.js
+++ src/session_record.js
@@ -190,7 +190,6 @@ class SessionRecord {
         let run = (data.version === undefined);
         for (let i = 0; i < migrations.length; ++i) {
             if (run) {
-                console.info("Migrating session to:", migrations[i].version);
                 migrations[i].migrate(data);
             } else if (migrations[i].version === data.version) {
                 run = true;
@@ -267,18 +266,14 @@ class SessionRecord {
 
     closeSession(session) {
         if (this.isClosed(session)) {
-            console.warn("Session already closed", session);
             return;
         }
-        console.info("Closing session:", session);
         session.indexInfo.closed = Date.now();
     }
 
     openSession(session) {
         if (!this.isClosed(session)) {
-            console.warn("Session already open");
         }
-        console.info("Opening session:", session);
         session.indexInfo.closed = -1;
     }
 
@@ -298,7 +293,6 @@ class SessionRecord {
                 }
             }
             if (oldestKey) {
-                console.info("Removing old closed session:", oldestSession);
                 delete this.sessions[oldestKey];
             } else {
                 throw new Error('Corrupt sessions object');
